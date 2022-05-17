5773f8c8af79ebe65559ea350d6774b349221fe8
coinlordd <coinlord.developer@gmail.com>
2022-05-17 13:41:05 +0200

    Remove Transfer + Sync event

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,15 +1,5 @@
-import {
-  Burn,
-  Mint,
-  Swap,
-  Sync,
-  Transfer,
-} from "../generated/DeusFtm/UniswapV2Pair";
+import { Burn, Mint, Swap } from "../generated/DeusFtm/UniswapV2Pair";
 import { snapshotPrice } from "./entities";
-
-export function handleBeetsSwap(event: Burn): void {
-  snapshotPrice(event);
-}
 
 export function handleBurn(event: Burn): void {
   snapshotPrice(event);
@@ -20,13 +10,5 @@ export function handleMint(event: Mint): void {
 }
 
 export function handleSwap(event: Swap): void {
-  snapshotPrice(event);
-}
-
-export function handleSync(event: Sync): void {
-  snapshotPrice(event);
-}
-
-export function handleTransfer(event: Transfer): void {
   snapshotPrice(event);
 }
